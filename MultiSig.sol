7e06ba591ed30ac61677a031a15da9d8cfb87ff7
Michael Chan <mikechansas@gmail.com>
2022-05-18 21:32:05 -0700

    Begin implementing logic for MultiSig.

--- MultiSig.sol
+++ MultiSig.sol
@@ -9,6 +9,9 @@ contract MultiSig {
     
     uint public votesRequired;
 
+     //Transaction Index => Owner => Bool denoting transaction approved by I'th Owner or not
+    mapping(uint => mapping(address => bool)) public ownerApproved;
+
     struct Transaction {
         address to;
         uint value;
@@ -18,7 +21,48 @@ contract MultiSig {
 
     Transaction[] public transactionHistory;
 
-    //Transaction Index => Owner => Bool denoting transaction approved by I'th Owner or not
-    mapping(uint => mapping(address => bool)) public ownerApproved;
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "Caller is not an owner!");
+        _;
+    }
 
+    constructor(address[] memory _owners, uint _votesRequired) {
+        require(_owners.length > 0, "Multisig must have at least one owner!");
+
+        for (uint i = 0; i < _owners.length; i++) { //Insert owners into state variables
+            address curOwner = _owners[i];
+            require(!isOwner[curOwner], "Owner already exists!");
+            
+            isOwner[curOwner] = true;
+            owners.push(curOwner);
+        }
+        votesRequired = _votesRequired;
+    }
+
+    receive() external payable {
+        
+    }
+
+    function submitTransaction(address _to, uint _valueSent, bytes calldata _data) external onlyOwner {
+        transactionHistory.push(Transaction({
+            to: _to,
+            value: _valueSent,
+            data: _data,
+            executed: false
+        }));
+    }
+
+    function approveTransaction(uint _txID) external onlyOwner {
+        ownerApproved[_txID][msg.sender] = true;
+    }
+
+    function txApprovalCount(uint _txID) private view returns (uint count) {
+        for (uint i = 0; i < owners.length; i++) {
+            if (ownerApproved[_txID][owners[i]]) {
+                count += 1;
+            }
+        }
+    }
+
+   
 }
