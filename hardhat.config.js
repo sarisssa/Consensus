1a38b2ebc4ed01efb5e4280be0d0dbb5db89debd
Michael Chan <mikechansas@gmail.com>
2022-06-29 18:29:08 -0700

    Install OZ Library and add gas optimizer into hardhat config.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,13 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.13",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
